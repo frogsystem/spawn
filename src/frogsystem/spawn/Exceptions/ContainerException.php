c5da2103616c537a9337a402526df46267d54cfb
Moritz Kornher <mail@moritzkornher.de>
2016-04-17 13:48:57 +1200

    phpDocs for exceptions

--- src/frogsystem/spawn/Exceptions/ContainerException.php
+++ src/frogsystem/spawn/Exceptions/ContainerException.php
@@ -4,7 +4,9 @@ namespace Frogsystem\Spawn\Exceptions;
 use Interop\Container\Exception\ContainerException as ContainerExceptionInterface;
 
 /**
+ * Class ContainerException
  * Base interface representing a generic exception in a container.
+ * @package Frogsystem\Spawn\Exceptions
  */
 class ContainerException extends \Exception implements ContainerExceptionInterface
 {
--- src/frogsystem/spawn/Exceptions/InvalidArgumentException.php
+++ src/frogsystem/spawn/Exceptions/InvalidArgumentException.php
@@ -2,7 +2,9 @@
 namespace Frogsystem\Spawn\Exceptions;
 
 /**
- * Passed argument is invalid as an abstract
+ * Class InvalidArgumentException
+ * Passed argument is invalid as an abstract.
+ * @package Frogsystem\Spawn\Exceptions
  */
 class InvalidArgumentException extends ContainerException
 {
